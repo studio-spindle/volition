384ccf53ffd98bef3eb74662034fa197356730a5
Vledder <remi@studiospindle.nl>
2020-06-17 17:30:39 +0200

    move nestjs to be folder

--- be/src/main.ts
+++ be/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  
+
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log(`Application listening on part ${port}`)
